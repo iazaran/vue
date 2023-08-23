d16beb489c675867684a2112c773efb8c30cbffa
iazaran <eazaran@gmail.com>
2023-08-23 14:12:57 +0400

    New components added  for Provide / Inject

---
+++ src/components/ComponentInjector18.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { inject } from 'vue'
+
+const { location, updateLocation } = inject('location')
+</script>
+
+<template>
+  <button class="bg-blue-100 border border-blue-500 rounded-md px-4 py-2"  @click="updateLocation">{{ location }}</button>
+</template>
---
+++ src/components/ComponentProvider18.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { provide, ref } from 'vue'
+import ComponentInjector18 from './ComponentInjector18.vue'
+
+const location = ref('North Pole')
+
+function updateLocation() {
+  location.value = 'South Pole'
+}
+
+provide('location', {
+  location,
+  updateLocation
+})
+</script>
+
+<template>
+  <h1 class="text-5xl mt-6">18</h1>
+  <div class="max-w-2xl lg:text-center border border-l-3 rounded border-gray-500 p-5">
+    <h2 class="text-2xl mb-5">Provide / Inject</h2>
+
+    <ComponentInjector18 />
+  </div>
+</template>
