549783984e11be84621b8515af75bf9c7e3fc0da
iazaran <eazaran@gmail.com>
2023-08-26 17:25:01 +0400

    Async component added

---
+++ src/components/Component19.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 class="text-5xl mt-6">19</h1>
+  <div class="max-w-2xl lg:text-center border border-l-3 rounded border-gray-500 p-5">
+    <h2 class="text-2xl mb-5">Async Component</h2>
+    
+    <p>In large applications, we may need to divide the app into smaller chunks and only load a component from the server when it's needed. To make that possible, Vue has a defineAsyncComponent function.</p>
+  </div>
+</template>
