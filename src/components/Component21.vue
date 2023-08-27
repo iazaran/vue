fb61edcac07fccbd4e8bad1d2af7a0c60c3c563e
iazaran <eazaran@gmail.com>
2023-08-27 11:55:56 +0400

    Custom Directives component added

---
+++ src/components/Component21.vue
@@ -0,0 +1,14 @@
+<script setup>
+const vPlaceholder = {
+  mounted: (el) => el.placeholder = 'Enter your name ...',
+}
+</script>
+
+<template>
+  <h1 class="text-5xl mt-6">21</h1>
+  <div class="max-w-2xl lg:text-center border border-l-3 rounded border-gray-500 p-5">
+    <h2 class="text-2xl mb-5">Custom Directives</h2>
+    
+    <input v-placeholder />
+  </div>
+</template>
