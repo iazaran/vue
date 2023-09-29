998cfc08c6fbce7f7ffa3e007f77a043fcce1f0d
Joe Hallinan <joe@vacayou.com>
2023-09-29 12:39:16 -0400

    Fix translations issue

--- src/utils/helpers/englishToFrench/englishToFrench.js
+++ src/utils/helpers/englishToFrench/englishToFrench.js
@@ -2,9 +2,7 @@ import { i18n } from '@/plugins'
 import { translations } from "@/content"
 
 const englishToFrench = (app) => {
-    app.use(i18n, {
-        translations
-    })
+    app.use(i18n, translations)
 }
 
 export { englishToFrench };
